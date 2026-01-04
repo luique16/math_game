d718b5755d6b8394643f784ac5cb2c4574bdf84e
Luís Henrique <luish.fa1515@gmail.com>
2026-01-03 21:22:53 -0300

    refactor: separating view components

---
+++ internal/view/components.go
@@ -0,0 +1,154 @@
+package view
+
+import (
+	"fmt"
+
+	"github.com/charmbracelet/lipgloss"
+
+	"github.com/luique16/math_game/internal/models"
+)
+
+func Hearts(lives int) string {
+	heartsStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#ff6961")).
+		Bold(true).
+		Margin(1).
+		Align(lipgloss.Center)
+
+	aliveHeartElementStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#ff0000")).
+		Bold(true).
+		Width(2).
+		Padding(0).
+		Align(lipgloss.Center)
+
+	deadHeartElementStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#666666")).
+		Bold(true).
+		Width(2).
+		Padding(0).
+		Align(lipgloss.Center)
+
+	var hearts []string
+
+	for range(lives) {
+		hearts = append(hearts, aliveHeartElementStyle.Render("❤ "))
+	}
+
+	for range(3 - lives) {
+		hearts = append(hearts, deadHeartElementStyle.Render("❤ "))
+	}
+
+	return heartsStyle.Render(lipgloss.JoinHorizontal(lipgloss.Top, hearts...))
+}
+
+func ColsSum(colsSums []int) string {
+	colsSumStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#ffffff")).
+		Bold(true).
+		Border(lipgloss.RoundedBorder(), true).
+		MarginLeft(7).
+		PaddingRight(2).
+		Align(lipgloss.Center)
+
+	colsElementStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#ffffff")).
+		PaddingLeft(2).
+		Width(4).
+		Align(lipgloss.Center)
+
+	var colsSum []string
+
+	for i := range(colsSums) {
+		colsSum = append(colsSum, colsElementStyle.Render(fmt.Sprintf("%d", colsSums[i])))
+	}
+
+	return colsSumStyle.Render(lipgloss.JoinHorizontal(lipgloss.Top, colsSum...))
+}
+
+func RowsSum(rowsSums []int) string {
+	rowsSumStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#ffffff")).
+		Bold(true).
+		Padding(0, 1, 1, 1).
+		Border(lipgloss.RoundedBorder(), true).
+		Align(lipgloss.Left)
+
+	rowElementStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#ffffff")).
+		PaddingTop(1).
+		Align(lipgloss.Center)
+
+	var rowsSum []string
+
+	for i := range(len(rowsSums)) {
+		rowsSum = append(rowsSum, rowElementStyle.Render(fmt.Sprintf("%d", rowsSums[i])))
+	}
+
+	return rowsSumStyle.Render(lipgloss.JoinVertical(lipgloss.Left, rowsSum...))
+}
+
+func Board(game *models.Game, cursorX, cursorY int) string {
+	boardStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#ffffff")).
+		Border(lipgloss.RoundedBorder(), true).
+		MarginLeft(1).
+		PaddingLeft(1).
+		PaddingRight(1).
+		PaddingTop(1).
+		Align(lipgloss.Center)
+
+	boardElementStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#ffffff")).
+		Width(4).
+		Padding(0).
+		MarginBottom(1).
+		Align(lipgloss.Center)
+
+	selectedElementStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#ff6961")).
+		Bold(true).
+		Width(4).
+		Padding(0).
+		MarginBottom(1).
+		Align(lipgloss.Center)
+
+	legitElementStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#666666")).
+		Width(4).
+		Padding(0).
+		MarginBottom(1).
+		Align(lipgloss.Center)
+
+
+	var board [][]string
+
+	for i := range(game.Rows) {
+		var row []string
+		for j := range(game.Cols) {
+			if cursorY == i && cursorX == j {
+				if game.Board[i][j].State != models.Erased {
+					row = append(row, selectedElementStyle.Render(fmt.Sprintf("%d", game.Board[i][j].Value)))
+				} else {
+					row = append(row, selectedElementStyle.Render("⚪ "))
+				}
+			} else if game.Board[i][j].Legit && game.Board[i][j].State == models.Selected {
+				row = append(row, legitElementStyle.Render(fmt.Sprintf("%d", game.Board[i][j].Value)))
+			} else if game.Board[i][j].State == models.Erased {
+				row = append(row, boardElementStyle.Render(" "))
+			} else {
+				row = append(row, boardElementStyle.Render(fmt.Sprintf("%d", game.Board[i][j].Value)))
+			}
+		}
+		board = append(board, row)
+	}
+
+	var boardRows []string
+
+	for i := range(game.Rows) {
+		boardRows = append(boardRows, lipgloss.JoinHorizontal(lipgloss.Left, board[i]...))
+	}
+
+	return boardStyle.Render(lipgloss.JoinVertical(lipgloss.Right, boardRows...))
+}
+
--- internal/view/view.go
+++ internal/view/view.go
@@ -1,170 +1,29 @@
 package view
 
 import (
-	"fmt"
-
 	"github.com/charmbracelet/lipgloss"
-
-	"github.com/luique16/math_game/internal/models"
 )
 
 func (v *View) View() string {
 	var print string
 
-	heartsStyle := lipgloss.NewStyle().
-		Foreground(lipgloss.Color("#ff6961")).
-		Bold(true).
-		Margin(1).
-		Align(lipgloss.Center)
-
-	aliveHeartElementStyle := lipgloss.NewStyle().
-		Foreground(lipgloss.Color("#ff0000")).
-		Bold(true).
-		Width(2).
-		Padding(0).
-		Align(lipgloss.Center)
-
-	deadHeartElementStyle := lipgloss.NewStyle().
-		Foreground(lipgloss.Color("#666666")).
-		Bold(true).
-		Width(2).
-		Padding(0).
-		Align(lipgloss.Center)
-
-	var hearts []string
-
-	for range(v.Game.Lives) {
-		hearts = append(hearts, aliveHeartElementStyle.Render("❤ "))
-	}
-
-	for range(3 - v.Game.Lives) {
-		hearts = append(hearts, deadHeartElementStyle.Render("❤ "))
-	}
-
-
 	print += lipgloss.JoinHorizontal(
 		lipgloss.Top, 
-		heartsStyle.Render(
-			lipgloss.JoinHorizontal(lipgloss.Top, hearts...),
-			),
+		Hearts(v.Game.Lives),
 		)
 
 	print += "\n"
-
-	colsSumStyle := lipgloss.NewStyle().
-		Foreground(lipgloss.Color("#ffffff")).
-		Bold(true).
-		Border(lipgloss.RoundedBorder(), true).
-		MarginLeft(7).
-		PaddingRight(2).
-		Align(lipgloss.Center)
-
-	colsElementStyle := lipgloss.NewStyle().
-		Foreground(lipgloss.Color("#ffffff")).
-		PaddingLeft(2).
-		Width(4).
-		Align(lipgloss.Center)
-
-	var colsSum []string
-
-	for i := range(v.Game.Cols) {
-		colsSum = append(colsSum, colsElementStyle.Render(fmt.Sprintf("%d", v.Game.ColsSums[i])))
-	}
-
-	rowsSumStyle := lipgloss.NewStyle().
-		Foreground(lipgloss.Color("#ffffff")).
-		Bold(true).
-		Padding(0, 1, 1, 1).
-		Border(lipgloss.RoundedBorder(), true).
-		Align(lipgloss.Left)
-
-	rowElementStyle := lipgloss.NewStyle().
-		Foreground(lipgloss.Color("#ffffff")).
-		PaddingTop(1).
-		Align(lipgloss.Center)
-
-	var rowsSum []string
-
-	for i := range(v.Game.Rows) {
-		rowsSum = append(rowsSum, rowElementStyle.Render(fmt.Sprintf("%d", v.Game.RowsSums[i])))
-	}
-
-	boardStyle := lipgloss.NewStyle().
-		Foreground(lipgloss.Color("#ffffff")).
-		Border(lipgloss.RoundedBorder(), true).
-		MarginLeft(1).
-		PaddingLeft(1).
-		PaddingRight(1).
-		PaddingTop(1).
-		Align(lipgloss.Center)
-
-	boardElementStyle := lipgloss.NewStyle().
-		Foreground(lipgloss.Color("#ffffff")).
-		Width(4).
-		Padding(0).
-		MarginBottom(1).
-		Align(lipgloss.Center)
-
-	selectedElementStyle := lipgloss.NewStyle().
-		Foreground(lipgloss.Color("#ff6961")).
-		Bold(true).
-		Width(4).
-		Padding(0).
-		MarginBottom(1).
-		Align(lipgloss.Center)
-
-	legitElementStyle := lipgloss.NewStyle().
-		Foreground(lipgloss.Color("#666666")).
-		Width(4).
-		Padding(0).
-		MarginBottom(1).
-		Align(lipgloss.Center)
-
-
-	var board [][]string
-
-	for i := range(v.Game.Rows) {
-		var row []string
-		for j := range(v.Game.Cols) {
-			if v.CursorY == i && v.CursorX == j {
-				if v.Game.Board[i][j].State != models.Erased {
-					row = append(row, selectedElementStyle.Render(fmt.Sprintf("%d", v.Game.Board[i][j].Value)))
-				} else {
-					row = append(row, selectedElementStyle.Render("⚪ "))
-				}
-			} else if v.Game.Board[i][j].Legit && v.Game.Board[i][j].State == models.Selected {
-				row = append(row, legitElementStyle.Render(fmt.Sprintf("%d", v.Game.Board[i][j].Value)))
-			} else if v.Game.Board[i][j].State == models.Erased {
-				row = append(row, boardElementStyle.Render(" "))
-			} else {
-				row = append(row, boardElementStyle.Render(fmt.Sprintf("%d", v.Game.Board[i][j].Value)))
-			}
-		}
-		board = append(board, row)
-	}
-
-	var boardRows []string
-
-	for i := range(v.Game.Rows) {
-		boardRows = append(boardRows, lipgloss.JoinHorizontal(lipgloss.Left, board[i]...))
-	}
 
 	print += lipgloss.JoinHorizontal(
 		lipgloss.Top, 
 		lipgloss.JoinVertical(
 			lipgloss.Left,
-			colsSumStyle.Render(
-				lipgloss.JoinHorizontal(lipgloss.Top, colsSum...),
-				),
+			ColsSum(v.Game.ColsSums),
 			lipgloss.JoinHorizontal(
 				lipgloss.Top,
-				rowsSumStyle.Render(
-					lipgloss.JoinVertical(lipgloss.Left, rowsSum...),
-					),
-				boardStyle.Render(
-					lipgloss.JoinVertical(lipgloss.Right, boardRows...),
-					),
-				),
+				RowsSum(v.Game.RowsSums),
+				Board(v.Game, v.CursorX, v.CursorY),
+			),
 		),
 	)
 
